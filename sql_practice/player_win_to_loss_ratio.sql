ec3fdac54b3fdef0d31e1a16e93db4cc8dea4cef
Kieran <79286428+kieran036@users.noreply.github.com>
2025-03-10 23:31:47 +0000

    Complete remaining parts of query

--- sql_practice/player_win_to_loss_ratio.sql
+++ sql_practice/player_win_to_loss_ratio.sql
@@ -5,7 +5,7 @@
  - players, with columns id (int) and pname varchar(30)
  - games, with columns id (int) and result varchar(5) as either 'won' or 'lost'.
  */
-WITH summary AS (
+WITH summary_1 AS (
     SELECT id,
         SUM (
             CASE
@@ -20,4 +20,22 @@ WITH summary AS (
             END
         ) AS losses
     FROM games
+    GROUP BY id
 ),
+summary_2 AS(
+    SELECT id,
+        ROUND (
+            CASE
+                WHEN losses = 0 THEN wins -- Avoid division by zero
+                ELSE wins * 1.00 / losses -- Convert to float
+            END,
+            2
+        ) AS ratio
+    FROM summary_1
+)
+SELECT p.pname AS player_name,
+    s.ratio AS ratio
+FROM summary_2
+    INNER JOIN players AS p ON summary_2.id = p.id
+ORDER BY ratio DESC,
+    player_name ASC;
