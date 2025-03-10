6f7c85bde86220a31c38ca185efbd05c55c0b858
Kieran <79286428+kieran036@users.noreply.github.com>
2025-03-10 21:57:44 +0000

    Add cities with more cust than avg sql query

---
+++ sql_practice/cities_with_more_cust_than_avg.sql
@@ -0,0 +1,24 @@
+/*
+ Three tables,
+ - country table with columns ID and country name
+ - city table with columns ID, city name, postal code, and country ID
+ - customer table with columns ID, customer name, city ID, customer address, contact person, email, and phone.
+ Write a query which will return all cities with more customers than the average number of customers of all cities. For each city, return the country name, city name, and the number of customers. Order the result by country name ascending.
+ */
+WITH avg_all AS (
+    SELECT COUNT(id) * 1.0 / COUNT(DISTINCT city_id) AS avg_customers
+    FROM customer
+),
+city_summary AS (
+    SELECT city_id,
+        COUNT(id) AS num_customers
+    FROM customer
+    GROUP BY city_id
+)
+SELECT city_id,
+    num_customers
+FROM city_summary AS cs
+WHERE num_customers > (
+        SELECT avg_customers
+        FROM avg_all
+    )
